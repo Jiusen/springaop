fc0932926df20149a0030013106d0f33938070c9
Guo Jiusen <2434732570@qq.com>
2020-10-17 11:34:39 +0800

    springaop01

---
+++ springaop01/src/main/java/com/example/springaop01/Springaop01Application.java
@@ -0,0 +1,13 @@
+package com.example.springaop01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springaop01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springaop01Application.class, args);
+    }
+
+}
---
+++ springaop01/src/test/java/com/example/springaop01/Springaop01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springaop01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springaop01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
